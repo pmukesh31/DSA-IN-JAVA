5ae3c054e4a89c1022f1523dd1ad7fd037658d0e
Mukesh Patel <110808838+pmukesh31@users.noreply.github.com>
2022-09-11 16:12:26 +0530

    Create Deque.java

---
+++ Queue/Deque/Deque.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Deque {
+    public static void main(String args[])
+    {
+        Deque <Integer> dq=new LinkedList<Integer>();
+        dq.offerFirst(10);
+        dq.offerLast(20);
+        dq.offerFirst(5);
+        dq.offerLast(15);
+        System.out.prinlnt(dq.peekFirst());
+        System.out.println(dq.peekLast());
+        dq.pollFirst();
+        dq.pollLast();
+        System.out.println(dq);
+    }
+    
+}
